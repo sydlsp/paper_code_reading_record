a691ef3487ae37d00b2e9a1899ccf39f3bdba1dd
sydlsp <1298750656@qq.com>
2024-04-18 11:44:05 +0800

    test

--- AniPortrait/ceshi/ResNetBlock2d_test.py
+++
@@ -1,3 +0,0 @@
-from diffusers.models.resnet import Downsample2D, ResnetBlock2D, Upsample2D
-
-resnet=ResnetBlock2D()
--- AniPortrait/ceshi/unet_2d_condition_test.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-from src.models.unet_2d_condition import UNet2DConditionModel
-path=path='/share/ckpt/shipu/ani/pretrained_model/reference_unet.pth'
-unet=UNet2DConditionModel.from_pretrained(
-        path,
-        subfolder="unet",
-    ).to(dtype=torch.float16, device="cuda")
